b6beb0031685fa6617c9901e7ca642fb77668a0b
Abhas Tandon <abhastandon007@gmail.com>
2015-10-08 13:28:09 -0700

    Add support for read and update to localstorage

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,12 +1,4 @@
-var rulesObj = {
-		":5601" : [
-			["body > div.content > div > div > dashboard-grid > ul > li:nth-child(10) > dashboard-panel", "rgb(87, 193, 123)", "rgb(191, 175, 64)"],
-			["body > div.content > div > div > dashboard-grid > ul > li:nth-child(7) > dashboard-panel", "rgb(87, 193, 123)", "rgb(191, 175, 64)"]
-		],
-		"7383" : [
-			[".nav.nav-tabs","rgb(66, 139, 202)","red"]
-		]
-	};
+var rulesObj = {};
 
 function replaceColorInSelector(selector, from, to) {
 	$(selector + " *").each(function(index) {
@@ -21,6 +13,8 @@ $(document).bind('DOMNodeInserted', replaceColorsInDom);
 
 function replaceColorsInDom(e) {
 	var i;
+	if ($.isEmptyObject(rulesObj))
+		return;
 	var ruleUris = Object.keys(rulesObj);
 	for (i = 0; i < ruleUris.length; i++) {
 		var match = window.location.href.match(RegExp(ruleUris[i]));
@@ -30,9 +24,9 @@ function replaceColorsInDom(e) {
 	}
 	if (i === ruleUris.length) 
 		return;
-	var rules = rulesObj[ruleUris[i]];
-    for (i = 0; i < rules.length; i++) {
-		replaceColorInSelector(rules[i][0], rules[i][1], rules[i][2]);
+
+ for (var ruleid in rulesObj[ruleUris[i]]) {
+		replaceColorInSelector(rulesObj[ruleUris[i]][ruleid][0], rulesObj[ruleUris[i]][ruleid][1], rulesObj[ruleUris[i]][ruleid][2]);
 	}
 }
 
@@ -42,17 +36,21 @@ function init() {
 	replaceColorsInDom();
 	$("body").append('<div id="recolorIt"></div>');
 	$("#recolorIt").append('<div class="recolorit-modal recolorit-container"></div>');
-	$(".recolorit-modal").append(getInputRowHtml(undefined, true));
-	$("#recolorIt .recolorit-modal").easyModal();
+	$("#recolorIt .recolorit-modal").append(getInputRowHtml(undefined, true));
+	$("#recolorIt .recolorit-modal").append('<div class="saved-rules"></div>');
+	$("#recolorIt .recolorit-modal").easyModal({top: 100});
 	$("#recolorIt .recolorit-modal").trigger('openModal');
+	$("#recolorIt").on('click' , ".delete-rule", deleteRecolorRule);
+	$("#recolorIt").on('click' , ".update-rule", updateRecolorRule);
+
 	getConfigFromLocalStorage();
 }
 
 function getInputRowHtml(values , isTop) {
 	if (!values || $.isEmptyObject(values)) {
-		values = {regex:"", csspath:"", from:"", to:""}
+		values = {ruleid:"", regex:"", csspath:"", from:"", to:""}
 	}
-	var html = '<div class="row insert-rule">\
+	var html = '<div class="row insert-rule" data-ruleid="' + values.ruleid + '" data-regex="' + values.regex + '">\
 										<div class="three columns">\
 											<input class="u-full-width regex" value="'+ values.regex + '" type="text" placeholder="RegExp for page in URL"/>\
 										</div>\
@@ -64,11 +62,73 @@ function getInputRowHtml(values , isTop) {
 										</div>\
 										<div class="two columns">\
 											<input class="u-full-width to" value="'+ values.to + '" type="text" placeholder="New color in rgb"/>\
-										</div>' + ((isTop) ? '<div class="two columns"><button class="button-primary save-new-rule">Save</button></div>' : '<div class="one column"><button class="button-primary update-rule">Update</button></div><div class="one column"><button class="button-primary delete-rule">DELETE</button></div>') + 
+										</div>' + ((isTop) ? ('<div class="two columns"><button class="button-primary save-new-rule">Save</button></div>') : ('<div class="one column"><button class="button-primary update-rule">Update</button></div><div class="one column"><button class="button-primary delete-rule">DELETE</button></div>')) + 
 						  '</div>'
 	return html;
 }
 
+function updateRecolorRule(e) {
+	var row = $(this).parent().parent();
+	var regex = row.attr("data-regex");
+	var ruleid = row.attr("data-ruleid");
+	delete rulesObj[regex][ruleid];
+
+	regex = row.find(".regex").val();
+	var csspath = row.find(".csspath").val();
+	var from = row.find(".from").val();
+	var to = row.find(".to").val();
+
+	rulesObj[regex][ruleid] = [csspath, from, to];
+	updateLocalStorage();
+}
+
+
+function deleteRecolorRule(e) {
+	var row = $(this).parent().parent();
+	var regex = row.attr("data-regex");
+	var ruleid = row.attr("data-ruleid");
+	delete rulesObj[regex][ruleid];
+	row.remove();
+	updateLocalStorage();
+}
+
+function updateLocalStorage() {
+	localStorage.setItem("rulesObj", JSON.stringify(rulesObj));
+}
+
 function getConfigFromLocalStorage() {
+	var i, html;
 	rulesObj = JSON.parse(localStorage.getItem("rulesObj"));
+	// rulesObj = {
+	// 	":5601": {
+	// 		"a": ["body > div.content > div > div > dashboard-grid > ul > li:nth-child(10) > dashboard-panel", "rgb(87, 193, 123)", "rgb(191, 175, 64)"],
+	// 		"b": ["body > div.content > div > div > dashboard-grid > ul > li:nth-child(7) > dashboard-panel", "rgb(87, 193, 123)", "rgb(191, 175, 64)"]
+	// 	},
+	// 	"7383": {
+	// 		"c": [".nav.nav-tabs","rgb(66, 139, 202)","red"]
+	// 	}
+	// 	,
+	// 	"wiki": {
+	// 		"d": ["#mp-topbanner","rgb(6, 69, 173)","red"]
+	// 	}	
+	// };
+	if (!$.isEmptyObject(rulesObj)) {
+		var ruleUris = Object.keys(rulesObj);
+		for (var ruleUri in rulesObj) {
+			for (var ruleid in rulesObj[ruleUri]) {
+				html = getInputRowHtml({ruleid: ruleid, regex: ruleUri, csspath: rulesObj[ruleUri][ruleid][0], from: rulesObj[ruleUri][ruleid][1], to: rulesObj[ruleUri][ruleid][2]}, false);
+				$("#recolorIt .saved-rules").append(html);	
+			}
+		}
+	}
+}
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
 }
